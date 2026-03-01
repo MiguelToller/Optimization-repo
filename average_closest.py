1fb047b5943095597ba8f505dce34e3a25abac59
Luiz Miguel Toller Marconatto <141162499+MiguelToller@users.noreply.github.com>
2026-02-28 21:00:23 -0300

    Add file 'funcao-linear.ipynb' & add comments/docs to functions.

--- average_closest.py
+++ average_closest.py
@@ -3,15 +3,28 @@ import random as rd
 list_num = []
 
 def list_generator():
+    """
+    Gera 50 numeros inteiros aleatorios entre 1 a 100 e armazena
+    esses valores na lista global 'lista_num'. 
+    Ao final, Imprime a lista gerada.
+    """
     for i in range(50):
         number = rd.randint(1, 100)
         list_num.append(number)
     print("LIST:", list_num)
 
 def media():
+    """
+    Calcula e retorna a media aritmetica dos valores armazenados
+    na lista global 'lista_num'.
+    """
     return sum(list_num) / len(list_num)
 
 def search_closest():
+    """
+    Encontra e retorna o valor da lista global 'list_num'
+    que esta mais proximo da media da propria lista.
+    """
     avg = media()
     closest = list_num[0]
     
@@ -25,12 +38,17 @@ def search_closest():
     return closest
 
 def show_closests():
+    """
+    Exibe na tela todos os valores da lista global 'list_num'
+    cuja diferença para o valor mais próximo da média seja
+    menor ou igual a 3.
+    """
     closest = search_closest()
     for i in range(len(list_num)):
         if abs(list_num[i] - closest) <= 3:
             print(list_num[i])
 
 list_generator()
-print("MEDIA:",media())
+print("MEDIA:", media())
 print("CLOSESTS:")
 show_closests()
